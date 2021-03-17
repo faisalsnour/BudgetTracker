86311cc880c9eb039a447198853d0ea2a4444cf1
Faisal Nour <faisalskhalifa@gmail.com>
2021-03-16 21:47:31 -0400

    function to create object store is created

--- public/db.js
+++ public/db.js
@@ -2,3 +2,18 @@ let db;
 // to create a database request
 const request = indexedDB.open("budget", 1);
 
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("The error is " + event.target.errorCode);
+};
